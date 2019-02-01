39c3d921dbb67a0f4755af7f069de5ff2da54e79
Anthony Doan <mythicalprogrammer@gmail.com>
2019-01-31 18:40:03 -0800

    added configuration to hash less in testing to make tests run faster.

--- chp8/rumbl/config/test.exs
+++ chp8/rumbl/config/test.exs
@@ -1,5 +1,7 @@
 use Mix.Config
 
+config :pbkdf2_elixir, :rounds, 1
+
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :rumbl, RumblWeb.Endpoint,
